39a942b291b9633289d7e24adf5c6f4e36c5787a
Amandeep Aulakh <amandeepaulakh00000@gmail.com>
2022-10-29 07:58:35 +0530

    login

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,17 +1,44 @@
-const User = require('../models/user');
-const catchAsyncError = require('../middleware/catchAsyncError');
+const User = require("../models/user");
+const errorHander = require("../utils/errorHandler");
+const sendToken = require("../utils/jwtToken");
+const catchAsyncError = require("../middleware/catchAsyncError");
 
-exports.registerUser = catchAsyncError( (req, res, next)=>{
+// register user
 
-    const { name, email, password, number } = req.body;
+exports.registerUser = catchAsyncError(async (req, res, next) => {
+  const { name, email, password, number } = req.body;
 
-    const user = User.create({
-        name, email, password, number
-    })
+  const user = await User.create({
+    name,
+    email,
+    password,
+    number,
+  });
 
-    res.status(200).json({
-        success: true,
-        User
-    });
+  sendToken(user, 201, res);
+});
 
-})
+//Login user
+
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  
+  if (!email || !password) {
+    return next(new errorHander("please enter email and password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new errorHander("invalid email or password", 401));
+  }
+
+  const isPasswordMatched = user.comparePassword("password");
+
+  if (!isPasswordMatched) {
+    return next(new errorHander("invalid email or password", 401));
+  }
+
+  sendToken(user, 201, res);
+});
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,28 +1,51 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please enter a name"],
+    minLength: [3, "please enter atleast 3 character"],
+  },
+  email: {
+    type: String,
+    required: [true, "please enter a email address"],
+    validator: [validator.isEmail, "please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "please enter a password"],
+    minLength: [7, "please enter atleast 7 characters password"],
+  },
+  number: {
+    type: Number,
+    required: [true, "please enter a number"],
+    minLength: [9, "please enter valid number"],
+  },
+});
 
-    name:{
-        type : String,
-        required: [ true, 'please enter a name'],
-        minLength:[3, 'please enter atleast 3 character']
-    },
-    email:{
-        type : String,
-        required: [ true, 'please enter a email address'],
-        validator: [ validator.isEmail, 'please enter a valid email']
-    },
-    password:{
-        type : String,
-        required: [ true, 'please enter a password'],
-        minLength : [7, 'please enter atleast 7 characters password']
-    },
-    number:{
-        type : Number,
-        required: [ true, 'please enter a number'],
-        minLength : [9, 'please enter valid number']
-    }
-})
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// jwt token
+
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SKEY, {
+    expiresIn: process.env.JWT_EXP,
+  });
+};
+
+// comparePassword
+
+userSchema.methods.comparePassword = async function (enterpassword) {
+  return await bcrypt.compare(enterpassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
